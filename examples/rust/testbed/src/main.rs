d77bf2eacc92a396d0569119787b0d5c41856cb1
Daniel Collin <daniel@collin.com>
2022-12-31 15:14:35 +0100

    Rust version show an image

--- examples/rust/testbed/src/main.rs
+++ examples/rust/testbed/src/main.rs
@@ -1,11 +1,27 @@
 use flowi::Application;
 use flowi::Flowi;
+use flowi::ui::WindowFlags;
 
-fn main_loop(ui: &Flowi) {
-    println!("Hello, world!");
+struct App {
+    image: flowi::image::Image,
+    flowi: Flowi,
+}
+
+fn main_loop(_flowi: &Flowi, app: &mut App) {
+    let ui = app.flowi.ui();
+
+    ui.window_begin("Hello, world!", WindowFlags::None);
+    ui.image(app.image);
+    ui.end();
 }
 
 fn main() {
     let flowi = Flowi::new("Test", "Test").unwrap();
-    Application::main_loop(main_loop);
+
+    let mut app = App {
+        image: flowi.image().create_from_file("/home/emoon/code/projects/rust_minifb/resources/uv.png").unwrap(),
+        flowi,
+    };
+
+    Application::main_loop_ud(&mut app, main_loop);
 }
