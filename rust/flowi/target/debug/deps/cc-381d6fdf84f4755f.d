5b539b629819b9a4d15e1ff4a9978b6bf8c7e484
Daniel Collin <daniel@collin.com>
2022-12-31 09:02:14 +0100

    cleanup

--- rust/flowi/target/debug/deps/cc-381d6fdf84f4755f.d
+++
@@ -1,8 +0,0 @@
-/home/emoon/code/projects/flowi/rust/flowi/target/debug/deps/cc-381d6fdf84f4755f.rmeta: /home/emoon/.cargo/registry/src/github.com-1ecc6299db9ec823/cc-1.0.72/src/lib.rs /home/emoon/.cargo/registry/src/github.com-1ecc6299db9ec823/cc-1.0.72/src/windows_registry.rs
-
-/home/emoon/code/projects/flowi/rust/flowi/target/debug/deps/libcc-381d6fdf84f4755f.rlib: /home/emoon/.cargo/registry/src/github.com-1ecc6299db9ec823/cc-1.0.72/src/lib.rs /home/emoon/.cargo/registry/src/github.com-1ecc6299db9ec823/cc-1.0.72/src/windows_registry.rs
-
-/home/emoon/code/projects/flowi/rust/flowi/target/debug/deps/cc-381d6fdf84f4755f.d: /home/emoon/.cargo/registry/src/github.com-1ecc6299db9ec823/cc-1.0.72/src/lib.rs /home/emoon/.cargo/registry/src/github.com-1ecc6299db9ec823/cc-1.0.72/src/windows_registry.rs
-
-/home/emoon/.cargo/registry/src/github.com-1ecc6299db9ec823/cc-1.0.72/src/lib.rs:
-/home/emoon/.cargo/registry/src/github.com-1ecc6299db9ec823/cc-1.0.72/src/windows_registry.rs:
