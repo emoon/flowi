f9f4e02ce268a6bfa11f70308eaf791f1b15a38c
Daniel Collin <daniel@collin.com>
2023-01-03 15:49:30 +0100

    Use Freetype2 for font generation

--- rust/flowi/build.rs
+++ rust/flowi/build.rs
@@ -10,6 +10,68 @@ fn add_includes(build: &mut cc::Build, root: &str, files: &[&str]) {
     build.includes(files.iter().map(|src| root.join(src)));
 }
 
+fn build_freetype2() {
+    let mut build = cc::Build::new();
+
+    build
+        .cpp(true)
+        .warnings(false)
+        .include(".")
+        .include("../../external/freetype2/include")
+        .define("FT2_BUILD_LIBRARY", None);
+
+    add_sources(
+        &mut build,
+        "../../external/freetype2/src",
+        &[
+            "autofit/autofit.c",
+            "base/ftbase.c",
+            "base/ftbbox.c",
+            "base/ftbdf.c",
+            "base/ftbitmap.c",
+            "base/ftcid.c",
+            "base/ftdebug.c",
+            "base/ftfstype.c",
+            "base/ftgasp.c",
+            "base/ftglyph.c",
+            "base/ftgxval.c",
+            "base/ftinit.c",
+            "base/ftmm.c",
+            "base/ftotval.c",
+            "base/ftpatent.c",
+            "base/ftpfr.c",
+            "base/ftstroke.c",
+            "base/ftsynth.c",
+            "base/ftsystem.c",
+            "base/fttype1.c",
+            "base/ftwinfnt.c",
+            "bdf/bdf.c",
+            "bzip2/ftbzip2.c",
+            "cache/ftcache.c",
+            "cff/cff.c",
+            "cid/type1cid.c",
+            "gzip/ftgzip.c",
+            "lzw/ftlzw.c",
+            "pcf/pcf.c",
+            "pfr/pfr.c",
+            "psaux/psaux.c",
+            "pshinter/pshinter.c",
+            "psnames/psnames.c",
+            "raster/raster.c",
+            "sdf/sdf.c",
+            "svg/svg.c",
+            "sfnt/sfnt.c",
+            "smooth/smooth.c",
+            "truetype/truetype.c",
+            "type1/type1.c",
+            "type42/type42.c",
+            "winfonts/winfnt.c",
+        ],
+    );
+
+    build.compile("freetype2");
+}
+
 fn main() {
     let mut build = cc::Build::new();
 
@@ -279,7 +341,7 @@ fn main() {
             "stb",
         ],
     );
-    add_includes(&mut build, flowi_root, &["include", "src"]);
+    add_includes(&mut build, flowi_root, &["include", "src", "external/freetype2/include"]);
 
     add_sources(
         &mut build,
@@ -308,6 +370,7 @@ fn main() {
             "external/dear-imgui/imgui_draw.cpp",
             "external/dear-imgui/imgui_tables.cpp",
             "external/dear-imgui/imgui_widgets.cpp",
+            "external/dear-imgui/misc/freetype/imgui_freetype.cpp",
             "external/nanosvg/nanosvg.c",
             "external/stb/stb.c",
         ],
@@ -330,4 +393,6 @@ fn main() {
     }
 
     build.compile("flowi");
+
+    build_freetype2();
 }
