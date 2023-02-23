cf98d656d6da311fa9a70e9aa5abf2489a3a5105
Daniel Collin <daniel@collin.com>
2023-02-23 18:44:23 +0100

    removed target files...

--- langs/rust/flowi/target/debug/build/flowi-23778dceaa102d52/build_script_build-23778dceaa102d52.d
+++
@@ -1,7 +0,0 @@
-/home/emoon/code/projects/flowi/langs/rust/flowi/target/debug/build/flowi-23778dceaa102d52/build_script_build-23778dceaa102d52: build.rs
-
-/home/emoon/code/projects/flowi/langs/rust/flowi/target/debug/build/flowi-23778dceaa102d52/build_script_build-23778dceaa102d52.d: build.rs
-
-build.rs:
-
-# env-dep:CLIPPY_ARGS=
--- langs/rust/flowi/target/debug/deps/jobserver-17a75fa7fe803e97.d
+++
@@ -1,8 +0,0 @@
-/home/emoon/code/projects/flowi/langs/rust/flowi/target/debug/deps/jobserver-17a75fa7fe803e97.rmeta: /home/emoon/.cargo/registry/src/github.com-1ecc6299db9ec823/jobserver-0.1.25/src/lib.rs /home/emoon/.cargo/registry/src/github.com-1ecc6299db9ec823/jobserver-0.1.25/src/unix.rs
-
-/home/emoon/code/projects/flowi/langs/rust/flowi/target/debug/deps/libjobserver-17a75fa7fe803e97.rlib: /home/emoon/.cargo/registry/src/github.com-1ecc6299db9ec823/jobserver-0.1.25/src/lib.rs /home/emoon/.cargo/registry/src/github.com-1ecc6299db9ec823/jobserver-0.1.25/src/unix.rs
-
-/home/emoon/code/projects/flowi/langs/rust/flowi/target/debug/deps/jobserver-17a75fa7fe803e97.d: /home/emoon/.cargo/registry/src/github.com-1ecc6299db9ec823/jobserver-0.1.25/src/lib.rs /home/emoon/.cargo/registry/src/github.com-1ecc6299db9ec823/jobserver-0.1.25/src/unix.rs
-
-/home/emoon/.cargo/registry/src/github.com-1ecc6299db9ec823/jobserver-0.1.25/src/lib.rs:
-/home/emoon/.cargo/registry/src/github.com-1ecc6299db9ec823/jobserver-0.1.25/src/unix.rs:
